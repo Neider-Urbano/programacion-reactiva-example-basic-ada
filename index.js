38e9e4af77ffa911b7dca27128d6043569ed5d7c
neider urbano <jjuly8julianur@gmail.com>
2023-11-20 11:18:57 -0500

    ajuste de logica de la actividad sobre programacion reactiva

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 import { of } from "rxjs";
 import { map } from "rxjs/operators";
-of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9).pipe();
+
+of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+  .pipe(
+    map((data) => {
+      if (data % 2 !== 0) {
+        return data;
+      }
+    })
+  )
+  .subscribe((data) => console.log(data));
